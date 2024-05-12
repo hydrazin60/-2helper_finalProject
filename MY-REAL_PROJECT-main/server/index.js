9599c55f16370fbcd71b8807ede33596e218ad5c
hydrazin60 <sureshkhdk45@gmail.com>
2024-05-12 22:55:19 +0545

    server side coding start ***##

--- MY-REAL_PROJECT-main/server/index.js
+++ MY-REAL_PROJECT-main/server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import router from "./routes/user.route.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,6 +13,8 @@ mongoose
 .catch((error) => {
   console.log(error);
 });
+
+app.use("/api/v1/user/" , router )
 
 app.listen(PORT, () => {
   console.log(`server running on port:${PORT}`);
