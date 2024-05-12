40c8af06c837d9efc1b09873cc894ec59a56c8f1
hydrazin60 <sureshkhdk45@gmail.com>
2024-05-12 22:44:55 +0545

    database  connected

--- MY-REAL_PROJECT-main/server/index.js
+++ MY-REAL_PROJECT-main/server/index.js
@@ -5,13 +5,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("MongoDb is Connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+.connect(process.env.MONGODB_URL)
+.then(() => {
+  console.log("MongoDb is Connected");
+})
+.catch((error) => {
+  console.log(error);
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port:${PORT}`);
